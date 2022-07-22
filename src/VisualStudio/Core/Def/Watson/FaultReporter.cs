backend-devloper/roslyn

commit 2147918422f18ca598b98fb7e862465db46f674e

Delete our SetCallstackIfEmpty helper
When an exception object is created but unthrown, the call stack is
simply null. This helper tries to "fix" that, but when it does
so the exception's StackTrace will not be a full stack trace, but
simply the frames that were unwound during the throw...which is just
this method.

Since this doesn't do anything useful, deleting it means the VS
Telemetry library will be able to do it's own handling around missing
stack traces, which it actually has code to deal with.

src/VisualStudio/Core/Def/Watson/FaultReporter.cs
@@ -94,7 +94,6 @@ namespace Microsoft.CodeAnalysis.ErrorReporting
                     return;
                 }
 
-                exception.SetCallstackIfEmpty();
                 var currentProcess = Process.GetCurrentProcess();
 
                 // write the exception to a log file:

src/VisualStudio/Core/Def/Watson/WatsonExtensions.cs
@@ -1,32 +1,0 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
-
-using System;
-
-namespace Microsoft.CodeAnalysis.ErrorReporting
-{
-    internal static class WatsonExtensions
-    {
-        public static void SetCallstackIfEmpty(this Exception exception)
-        {
-            // There have been cases where a new, unthrown exception has been passed to this method.
-            // In these cases the exception won't have a stack trace, which isn't very helpful. We
-            // throw and catch the exception here as that will result in a stack trace that is
-            // better than nothing.
-            if (exception.StackTrace != null)
-            {
-                return;
-            }
-
-            try
-            {
-                throw exception;
-            }
-            catch
-            {
-                // Empty; we just need the exception to have a stack trace.
-            }
-        }
-    }
-}

src/Workspaces/Remote/ServiceHub/Microsoft.CodeAnalysis.Remote.ServiceHub.csproj
@@ -34,7 +34,6 @@
   </ItemGroup>
   <ItemGroup>
     <Compile Include="..\..\..\VisualStudio\Core\Def\UnusedReferences\ProjectAssets\ProjectAssetsFileReader.cs" Link="Services\UnusedReferences\ProjectAssets\ProjectAssetsFileReader.cs" />
-    <Compile Include="..\..\..\VisualStudio\Core\Def\Watson\WatsonExtensions.cs" Link="Services\ProcessTelemetry\WatsonExtensions.cs" />
     <Compile Include="..\..\..\VisualStudio\Core\Def\Watson\FaultReporter.cs" Link="Services\ProcessTelemetry\FaultReporter.cs" />
     <Compile Include="..\..\..\VisualStudio\Core\Def\Storage\AbstractCloudCachePersistentStorageService.cs" Link="Host\Storage\AbstractCloudCachePersistentStorageService.cs" />
     <Compile Include="..\..\..\VisualStudio\Core\Def\Storage\CloudCachePersistentStorage.cs" Link="Host\Storage\CloudCachePersistentStorage.cs" />

commit a86ccfdfe713afbb4eb717133f9a6a5b752a7d43

Ensure we don't throw first-chance exceptions if we have a null stack
This was introduced by 2147918422f18ca598b98fb7e862465db46f674e:
now we may have a null stack on the exception, which it turns out
means new StackTrace(exception).GetFrames() will return null, even
though it's not documented as doing that.

Thankfully this was being swallowed right way, but still not great.

src/VisualStudio/Core/Def/Watson/FaultReporter.cs
@@ -157,28 +157,36 @@ namespace Microsoft.CodeAnalysis.ErrorReporting
             try
             {
                 // walk up the stack looking for the first call from a type that isn't in the ErrorReporting namespace.
-                foreach (var frame in new StackTrace(exception).GetFrames())
+                var frames = new StackTrace(exception).GetFrames();
+
+                // On the .NET Framework, GetFrames() can return null even though it's not documented as such.
+                // At least one case here is if the exception's stack trace itself is null.
+                if (frames != null)
                 {
-                    var method = frame?.GetMethod();
-                    var methodName = method?.Name;
-                    if (methodName == null)
-                        continue;
+                    foreach (var frame in frames)
+                    {
+                        var method = frame?.GetMethod();
+                        var methodName = method?.Name;
+                        if (methodName == null)
+                            continue;
 
-                    var declaringTypeName = method?.DeclaringType?.FullName;
-                    if (declaringTypeName == null)
-                        continue;
+                        var declaringTypeName = method?.DeclaringType?.FullName;
+                        if (declaringTypeName == null)
+                            continue;
 
-                    if (!declaringTypeName.StartsWith(CodeAnalysisNamespace))
-                        continue;
+                        if (!declaringTypeName.StartsWith(CodeAnalysisNamespace))
+                            continue;
 
-                    return declaringTypeName + "." + methodName;
+                        return declaringTypeName + "." + methodName;
+                    }
                 }
             }
             catch
             {
             }
 
-            return "Roslyn NonFatal Watson";
+            // If we couldn't get a stack, do this
+            return exception.Message;
         }
 
         private static List<string> CollectServiceHubLogFilePaths()